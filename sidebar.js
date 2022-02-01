1e439b811674be00ec43f76e0db467bbf2b67729
Corentin <tintinsoyoca@gmail.com>
2022-02-01 21:22:10 +0100

    V1.1

--- sidebar.js
+++ sidebar.js
@@ -13,3 +13,14 @@ let toggleNav=function(){
     }
 
 }
+
+let test=function(){
+    if(window.innerWidth>600){
+        let getSidebar= document.querySelector(".sidebar")
+        getSidebar.style.visibility = "visible";
+        toggleNavStatus=true;
+    }
+}
+
+let previous = window.innerWidth
+window.onresize=test
